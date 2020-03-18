70e71044fcfbd4479e6fdd4842812584d4033a91
utkarsh bhardwaj <visitutkarsh634@gmail.com>
2020-03-19 01:17:21 +0530

    sample app

---
+++ app.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+
+from scripts import tabledef
+from scripts import forms
+from scripts import helpers
+from flask import Flask, redirect, url_for, render_template, request, session
+import json
+import sys
+import os
+
+import os
+import sys
+import urlparse
+
+# Register database schemes in URLs.
+urlparse.uses_netloc.append('mysql')
+
+try:
+
+    # Check to make sure DATABASES is set in settings.py file.
+    # If not default to {}
+
+    if 'DATABASES' not in locals():
+        DATABASES = {}
+
+    if 'DATABASE_URL' in os.environ:
+        url = urlparse.urlparse(os.environ['DATABASE_URL'])
+
+        # Ensure default database exists.
+        DATABASES['default'] = DATABASES.get('default', {})
+
+        # Update with environment configuration.
+        DATABASES['default'].update({
+            'NAME': url.path[1:],
+            'USER': url.username,
+            'PASSWORD': url.password,
+            'HOST': url.hostname,
+            'PORT': url.port,
+        })
+
+
+        if url.scheme == 'mysql':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+except Exception:
+    print('Unexpected error:', sys.exc_info())
+app = Flask(__name__)
+app.secret_key = os.urandom(12)  # Generic key for dev purposes only
+
+# Heroku
+#from flask_heroku import Heroku
+#heroku = Heroku(app)
+
+# ======== Routing =========================================================== #
+# -------- Login ------------------------------------------------------------- #
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if not session.get('logged_in'):
+        form = forms.LoginForm(request.form)
+        if request.method == 'POST':
+            username = request.form['username'].lower()
+            password = request.form['password']
+            if form.validate():
+                if helpers.credentials_valid(username, password):
+                    session['logged_in'] = True
+                    session['username'] = username
+                    return json.dumps({'status': 'Login successful'})
+                return json.dumps({'status': 'Invalid user/pass'})
+            return json.dumps({'status': 'Both fields required'})
+        return render_template('login.html', form=form)
+    user = helpers.get_user()
+    return render_template('home.html', user=user)
+
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return redirect(url_for('login'))
+
+
+# -------- Signup ---------------------------------------------------------- #
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if not session.get('logged_in'):
+        form = forms.LoginForm(request.form)
+        if request.method == 'POST':
+            username = request.form['username'].lower()
+            password = helpers.hash_password(request.form['password'])
+            email = request.form['email']
+            if form.validate():
+                if not helpers.username_taken(username):
+                    helpers.add_user(username, password, email)
+                    session['logged_in'] = True
+                    session['username'] = username
+                    return json.dumps({'status': 'Signup successful'})
+                return json.dumps({'status': 'Username taken'})
+            return json.dumps({'status': 'User/Pass required'})
+        return render_template('login.html', form=form)
+    return redirect(url_for('login'))
+
+
+# -------- Settings ---------------------------------------------------------- #
+@app.route('/settings', methods=['GET', 'POST'])
+def settings():
+    if session.get('logged_in'):
+        if request.method == 'POST':
+            password = request.form['password']
+            if password != "":
+                password = helpers.hash_password(password)
+            email = request.form['email']
+            helpers.change_user(password=password, email=email)
+            return json.dumps({'status': 'Saved'})
+        user = helpers.get_user()
+        return render_template('settings.html', user=user)
+    return redirect(url_for('login'))
+
+
+# ======== Main ============================================================== #
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=True, host="0.0.0.0")
